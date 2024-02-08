4f36e2967a56a93d33b5036bb7b3bf07d6b9428d
Mohammed Fakih <mhd.fakih@gmail.com>
2024-02-08 22:16:49 +0300

    prepartionTime compo done

---
+++ src/components/preparationTime/PreparationTime.jsx
@@ -0,0 +1,22 @@
+import "./preparationTime.css";
+
+const PreparationTime = () => {
+  return (
+    <section className="preparation">
+      <h2> Preparation time</h2>
+      <ul>
+        <li>
+          <span><b>Total:</b> Approximately 10 minutes</span>
+        </li>
+        <li>
+          <span><b>Preparation:</b> 5 minutes</span>
+        </li>
+        <li>
+          <span><b>Cooking:</b> 5 minutes</span>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default PreparationTime;
