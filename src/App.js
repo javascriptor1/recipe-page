e33708736fbf44841c11f16396d1186266f64bfc
Mohammed Fakih <mhd.fakih@gmail.com>
2024-02-09 18:06:33 +0300

    nutrition compo done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-
-import HeadingTitle from './components/heading/HeadingTitle.jsx'
-import PreparationTime from './components/preparationTime/PreparationTime.jsx'
-import Ingredients from './components/ingredients/Ingredients.jsx'
-import Instructions from './components/instructions/Instructions.jsx'
+import HeadingTitle from "./components/heading/HeadingTitle.jsx";
+import PreparationTime from "./components/preparationTime/PreparationTime.jsx";
+import Ingredients from "./components/ingredients/Ingredients.jsx";
+import Instructions from "./components/instructions/Instructions.jsx";
+import Nutrition from "./components/nutrition/Nutrition.jsx";
 import "./App.css";
 
 function App() {
   return (
     <main>
-      <HeadingTitle/>
-      <PreparationTime/>
-      <Ingredients/>
-      <Instructions/>
+      <HeadingTitle />
+      <PreparationTime />
+      <Ingredients />
+      <Instructions />
+      <Nutrition />
     </main>
   );
 }
--- src/components/nutrition/Nutrition.jsx
+++ src/components/nutrition/Nutrition.jsx
@@ -1,1 +1,42 @@
-    
+import "./nutrition.css";
+const nutritionTable = [
+  { id: 0, nutrition: "Calories", value: "277kcal" },
+  { id: 1, nutrition: "Carbs", value: "0g" },
+  { id: 2, nutrition: "Protein", value: "20g" },
+  { id: 3, nutrition: "Fat", value: "22g" },
+];
+
+const Nutrition = () => {
+  return (
+    <section className="nutrition">
+      <h3>Nutrition</h3>
+      <p>
+        The table below shows nutritional values per serving without the
+        additional fillings.
+      </p>
+      <table>
+        <tbody>
+          {nutritionTable.map((tableContent) => {
+            return (
+              <NutritionItem
+                tableContent={tableContent}
+                key={tableContent.id}
+              />
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+const NutritionItem = ({ tableContent }) => {
+  return (
+    <tr>
+      <td key={tableContent.nutrition}>{tableContent.nutrition}</td>
+      <td key={tableContent.value}>{tableContent.value}</td>
+    </tr>
+  );
+};
+
+export default Nutrition;
